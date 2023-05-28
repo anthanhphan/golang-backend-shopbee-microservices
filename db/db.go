950174574e4f6129d8b6779ee7bbd4ebd2b40cf3
Phan Thanh An <thanhanphan17@gmail.com>
2023-05-28 17:17:44 +0700

    [FEATURE] add signin and signup features

---
+++ db/db.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func ConnectDB(dsn string) *gorm.DB {
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("[ERROR] %s", err)
+
+		return db
+	}
+
+	return db
+}
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"shopbee/db"
+	"shopbee/handler"
+	"shopbee/model"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	LoadEnv()
+}
+
+func main() {
+	user := model.User{}
+
+	fmt.Print(user)
+
+	db := db.ConnectDB(os.Getenv("MYSQL_CONN"))
+
+	fmt.Print(db)
+
+	router := gin.Default()
+
+	v1 := router.Group("v1")
+	{
+		v1.POST("sign-up", handler.Signup(db))
+		v1.POST("sign-in", handler.Signin(db))
+	}
+
+	router.Run(":3000")
+}
+
+func LoadEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("[ERROR] %s", err)
+	}
+}
