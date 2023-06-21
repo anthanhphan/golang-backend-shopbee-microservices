3e5b5c7a9b4112e5775d425c660814b766dc83aa
Phan Thanh An <thanhanphan17@gmail.com>
2023-06-21 18:29:27 +0700

    [FIX] fix proxy not found

--- api-gateway/server.ts
+++ api-gateway/server.ts
@@ -6,22 +6,45 @@ const defaultBackendURL = "http://localhost:8000";
 
 // Create API gateway server
 const server = http.createServer((req, res) => {
-  // Select backend server based on the requested route
-  const route = Object.keys(routes).find((r) => req.url.startsWith(r));
-  const backendURL = route ? routes[route] : defaultBackendURL;
+    // Select backend server based on the requested route
+    const route = Object.keys(routes).find((r) => req.url.startsWith(r));
+    const backendURL = route ? routes[route] : defaultBackendURL;
 
-  // Create a proxy request to the selected backend server
-  const proxyReq = http.request(backendURL, { method: req.method, headers: req.headers }, (proxyRes) => {
-    // Forward the response from the backend server to the client
-    res.writeHead(proxyRes.statusCode, proxyRes.headers);
-    proxyRes.pipe(res);
-  });
+    if (!backendURL) {
+        // No matching route found, send a 404 response
+        res.statusCode = 404;
+        res.end("Not Found");
+        return;
+    }
 
-  // Forward the client request body to the backend server
-  req.pipe(proxyReq);
+    // Create a proxy request to the selected backend server
+    const proxyReq = http.request(
+        backendURL,
+        { method: req.method, headers: req.headers },
+        (proxyRes) => {
+            // Forward the response from the backend server to the client
+            res.writeHead(proxyRes.statusCode, proxyRes.headers);
+            proxyRes.pipe(res);
+        }
+    );
+
+    // Forward the client request body to the backend server
+    req.pipe(proxyReq);
+
+    // Error handler for the proxy request
+    proxyReq.on("error", (err) => {
+        console.error("Proxy Request Error:", err);
+        res.statusCode = 500;
+        res.end("Proxy Request Error");
+    });
 });
 
-const port = 3000;
+// Error handler for the server
+server.on("error", (err) => {
+    console.error("Server Error:", err);
+});
+
+const port = 80;
 server.listen(port, () => {
-  console.log(`Shopbee API gateway is running on port ${port}`);
+    console.log(`Shopbee API gateway is running on port ${port}`);
 });
