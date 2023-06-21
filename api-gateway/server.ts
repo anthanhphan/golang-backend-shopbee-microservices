66e12af29353302bf7043edf081a990a96b90316
Phan Thanh An <thanhanphan17@gmail.com>
2023-06-21 18:23:32 +0700

    [FIX] fix error middleware proxy

--- api-gateway/server.ts
+++ api-gateway/server.ts
@@ -1,34 +1,27 @@
 const http = require("http");
 const routes = require("./routes.json");
 
-const { createProxyMiddleware } = require("http-proxy-middleware");
-
-// Define API routes and corresponding backend services
-// const { routes } = require("./routes.js");
-
 // Specify the default backend server URL for invalid routes
 const defaultBackendURL = "http://localhost:8000";
 
-// Create proxy middleware
-const proxy = createProxyMiddleware({
-    changeOrigin: true,
-    xfwd: true,
-    router: (req) => {
-        // Select backend server based on the requested route
-        const route = Object.keys(routes).find((r) => req.url.startsWith(r));
-        if (route) {
-            return routes[route];
-        }
-        return defaultBackendURL; // Use the default backend URL for invalid routes
-    },
-});
-
 // Create API gateway server
 const server = http.createServer((req, res) => {
-    proxy(req, res);
+  // Select backend server based on the requested route
+  const route = Object.keys(routes).find((r) => req.url.startsWith(r));
+  const backendURL = route ? routes[route] : defaultBackendURL;
+
+  // Create a proxy request to the selected backend server
+  const proxyReq = http.request(backendURL, { method: req.method, headers: req.headers }, (proxyRes) => {
+    // Forward the response from the backend server to the client
+    res.writeHead(proxyRes.statusCode, proxyRes.headers);
+    proxyRes.pipe(res);
+  });
+
+  // Forward the client request body to the backend server
+  req.pipe(proxyReq);
 });
 
 const port = 3000;
 server.listen(port, () => {
-    console.log(`Shopbee API gateway is running on port ${port}`);
+  console.log(`Shopbee API gateway is running on port ${port}`);
 });
