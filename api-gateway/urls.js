30f011f1019ac4571233ab71b2ab73ac5034a6bf
Phan Thanh An <thanhanphan17@gmail.com>
2023-08-06 16:23:59 +0700

    update urls discovery

--- api-gateway/urls.js
+++ api-gateway/urls.js
@@ -1,8 +1,10 @@
 const urls = [
     "http://13.54.238.78:8080/discover/user",
     "http://13.54.238.78:8080/discover/product",
+    "http://13.54.238.78:8080/discover/category",
     "http://13.54.238.78:8080/discover/upload",
     "http://13.54.238.78:8080/discover/mail",
+    "http://13.54.238.78:8080/discover/request",
 ];
 
 module.exports = urls;
