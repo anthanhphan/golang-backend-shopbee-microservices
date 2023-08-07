8df9c5625394dde382edc7bbed8be843b0e0ff90
Phan Thanh An <thanhanphan17@gmail.com>
2023-08-07 16:48:27 +0700

    fix urls

--- api-gateway/urls.js
+++ api-gateway/urls.js
@@ -2,7 +2,7 @@ const urls = [
     "http://shopbee-api.shop:8080/discover/user",
     "http://shopbee-api.shop:8080/discover/product",
     "http://shopbee-api.shop:8080/discover/category",
-    "http://shopbee-api.shop:8080//discover/upload",
+    "http://shopbee-api.shop:8080/discover/upload",
     "http://shopbee-api.shop:8080/discover/mail",
     "=http://shopbee-api.shop:8080/discover/request",
 ];
