36a685fb8d7b91ff00812124caf55f39d29e71a3
Phan Thanh An <thanhanphan17@gmail.com>
2023-08-07 16:08:36 +0700

    update api gateway url

--- api-gateway/urls.js
+++ api-gateway/urls.js
@@ -1,10 +1,10 @@
 const urls = [
-    "http://13.54.238.78:8080/discover/user",
-    "http://13.54.238.78:8080/discover/product",
-    "http://13.54.238.78:8080/discover/category",
-    "http://13.54.238.78:8080/discover/upload",
-    "http://13.54.238.78:8080/discover/mail",
-    "http://13.54.238.78:8080/discover/request",
+    "http://shopbee-api.shop:8080/discover/user",
+    "http://shopbee-api.shop:8080/discover/product",
+    "http://shopbee-api.shop:8080/discover/category",
+    "http://shopbee-api.shop:8080//discover/upload",
+    "http://shopbee-api.shop:8080/discover/mail",
+    "=http://shopbee-api.shop:8080/discover/request",
 ];
 
 module.exports = urls;
