630420f032e385502f8a75d7c7096ba7925a6ef1
Phan Thanh An <thanhanphan17@gmail.com>
2023-08-07 16:48:41 +0700

    fix urls

--- api-gateway/urls.js
+++ api-gateway/urls.js
@@ -4,7 +4,7 @@ const urls = [
     "http://shopbee-api.shop:8080/discover/category",
     "http://shopbee-api.shop:8080/discover/upload",
     "http://shopbee-api.shop:8080/discover/mail",
-    "=http://shopbee-api.shop:8080/discover/request",
+    "http://shopbee-api.shop:8080/discover/request",
 ];
 
 module.exports = urls;
