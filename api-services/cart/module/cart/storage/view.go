f7e88124338901d53b819c397feef238a891ace8
thanhanphan17 <thanhanphan17@gmail.com>
2023-08-12 11:56:29 +0700

    update view response data

--- api-services/cart/module/cart/storage/view.go
+++ api-services/cart/module/cart/storage/view.go
@@ -1,6 +1,9 @@
 package cartstorage
 
-import "context"
+import (
+	"context"
+	"shopbee/common"
+)
 
 func (s *cartMySql) ViewMyCart(
 	ctx context.Context,
@@ -10,7 +13,7 @@ func (s *cartMySql) ViewMyCart(
 	db := s.db
 
 	rows, err := db.Table("carts").
-		Select("users.id as shop_id, users.fullname as shop_name, products.id as product_id").
+		Select("users.id as shop_id, users.fullname as shop_name, users.avatar as shop_avatar, products.id as product_id").
 		Joins("JOIN products ON carts.product_id = products.id").
 		Joins("JOIN users ON products.shop_id = users.id").
 		Where("carts.user_id = ?", userId).
@@ -25,8 +28,9 @@ func (s *cartMySql) ViewMyCart(
 	for rows.Next() {
 		var shopID int
 		var shopName string
+		var shopAvatar *common.Image
 		var productID int
-		err := rows.Scan(&shopID, &shopName, &productID)
+		err := rows.Scan(&shopID, &shopName, &shopAvatar, &productID)
 		if err != nil {
 			return nil, err
 		}
@@ -36,6 +40,7 @@ func (s *cartMySql) ViewMyCart(
 			shopData[shopID] = map[string]interface{}{
 				"shop_id":     shopID,
 				"shop_name":   shopName,
+				"shop_avatar": shopAvatar,
 				"product_ids": []int{productID},
 			}
 		} else {
