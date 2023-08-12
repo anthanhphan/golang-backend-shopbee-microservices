296a9485b51151b9ae58da6c392ca62da470660a
Phan Thanh An <87409474+thanhanphan17@users.noreply.github.com>
2023-08-12 14:26:32 +0700

    update urls.js

--- api-gateway/urls.js
+++ api-gateway/urls.js
@@ -7,6 +7,9 @@ const urls = [
     "http://shopbee-api.shop:8080/discover/request",
     "http://shopbee-api.shop:8080/discover/wishlist",
     "http://shopbee-api.shop:8080/discover/cart",
+    "http://shopbee-api.shop:8080/discover/admin",
+    "http://shopbee-api.shop:8080/discover/payment",
+    "http://shopbee-api.shop:8080/discover/order",
 ];
 
 module.exports = urls;
